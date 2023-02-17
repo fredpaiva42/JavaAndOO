7e8696a00316b789d6b48532c326a6bfa53c4784
fredpaiva42 <fredericogdpaiva@gmail.com>
2023-02-17 16:49:27 -0300

    Curso Finalizado

---
+++ src/sintaxeBasica/TestaCaracteres.java
@@ -0,0 +1,10 @@
+package sintaxeBasica;
+
+public class TestaCaracteres {
+    public static void main(String[] args){
+        char letra = 65;
+        System.out.println(letra);
+        String palavra = "Frederico Gomes de Paiva";
+        System.out.println(palavra);
+    }
+}
